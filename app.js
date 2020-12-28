fe833426b77b315dc79bab54ce9ef23d4db45a17
Audra <73372336+IamAudra@users.noreply.github.com>
2020-12-28 16:56:33 -0500

    Initial commit for sliding nav bar in JS

---
+++ app.js
@@ -0,0 +1,27 @@
+const tableMenu = document.getElementById(elementId:'book-table')
+const navBar = documet.getElementById(elementId: 'nav-bar')
+
+
+
+
+tableMenu.addEventListener( type: 'click', listener: function() {
+function opennav-navBar() {
+    document.getElementById('nav-bar');
+}
+function closenavBar() {
+    document.getElementById('nav-bar');
+}
+    console.log(document.body)
+    //add transform translate-x-96
+    //
+   // document.body.classList.add('class-3')
+    //document.body.classList.remove('class-2')
+
+    document.body.classList.toggle(token:'nav-bar')
+
+    //tableMenu.classList.add('some-random-class')
+   // console.log(document.body. classList)
+   // console.log(TableMenu.classList)
+  
+})
+
